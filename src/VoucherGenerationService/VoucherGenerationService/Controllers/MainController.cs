8ff53f16357e9a6c8a3bbec0b10c5c1f9ca1ef94
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2020-08-09 08:22:18 +0200

    Full draft of voucher generation

--- src/VoucherGenerationService/VoucherGenerationService/Controllers/MainController.cs
+++ src/VoucherGenerationService/VoucherGenerationService/Controllers/MainController.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Org.BouncyCastle.OpenSsl;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Text;
 using System.Threading.Tasks;
 using WomPlatform.Connector;
+using WomPlatform.Connector.Models;
+using WomPlatform.Web.Generator.ViewModels;
 
 namespace WomPlatform.Web.Generator.Controllers {
-    
+
     [Route("")]
     public class MainController : Controller {
 
@@ -30,20 +36,26 @@ namespace WomPlatform.Web.Generator.Controllers {
         [HttpGet("source")]
         public async Task<IActionResult> ShowInstruments() {
             var session = HttpContext.Session.GetObject<SessionInfo>(Program.SessionKeyLogin);
+            var login = await _client.LoginAsSource(session.Username, session.Password);
 
-            var result = await _client.LoginAsMerchant(session.Username, session.Password);
-
-            return View("Instruments", result);
+            return View("Instruments", login);
         }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
 
         [RequireSession(Program.SessionKeyLogin)]
         [HttpGet("source/{sourceId}")]
-        public IActionResult ShowGenerationForm(
+        public async Task<IActionResult> ShowGenerationForm(
             [FromRoute] string sourceId
         ) {
             ViewBag.SourceId = sourceId;
 
-            return View("SourceForm");
+            var aims = await _client.GetAims();
+
+            return View("SourceForm", new GenerationFormViewModel {
+                Aims = aims.Aims
+            });
         }
 
         [RequireSession(Program.SessionKeyLogin)]
@@ -54,10 +66,75 @@ namespace WomPlatform.Web.Generator.Controllers {
             [FromForm] int voucherCount,
             [FromForm] double latitude,
             [FromForm] double longitude,
-            [FromForm] DateTime timestamp,
+            [FromForm] string date,
+            [FromForm] string time,
             [FromForm] string aimCode
         ) {
-            return Content("ALALAL");
+            if(sourceId == null) {
+                _logger.LogError("SourceID not set");
+                return RedirectToAction(nameof(ShowInstruments));
+            }
+            if(genCount <= 0) {
+                ErrorMessage = "Generation count cannot be 0 or negative";
+                return RedirectToAction(nameof(ShowGenerationForm), new { sourceId });
+            }
+            if(voucherCount <= 0) {
+                ErrorMessage = "Number of vouchers cannot be 0 or negative";
+                return RedirectToAction(nameof(ShowGenerationForm), new { sourceId });
+            }
+
+            var tsString = $"{date}T{time}";
+            if(!DateTime.TryParseExact(tsString, "yyyy-MM-ddTHH:mm", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal, out var timestamp)) {
+                _logger.LogError("Timestamp string {0} not parsable", tsString);
+
+                ErrorMessage = "Date or time not set correctly";
+                return RedirectToAction(nameof(ShowGenerationForm), new { sourceId });
+            }
+
+            var session = HttpContext.Session.GetObject<SessionInfo>(Program.SessionKeyLogin);
+            var login = await _client.LoginAsSource(session.Username, session.Password);
+
+            string key = GetKey(login, sourceId);
+            if(key == null) {
+                _logger.LogError("SourceID does not match logged-in user's authorized sources");
+                return RedirectToAction(nameof(ShowInstruments));
+            }
+
+            using var keyStream = new MemoryStream(Encoding.ASCII.GetBytes(key));
+            var instrument = _client.CreateInstrument(sourceId, keyStream);
+
+            _logger.LogInformation("Requesting {0} vouchers for aim {1}, in {2:F3}x{3:F3} at {4} ({5} reps)",
+                voucherCount, aimCode, latitude, longitude, timestamp, genCount);
+
+            var voucherInfo = new VoucherCreatePayload.VoucherInfo {
+                Aim = aimCode,
+                Count = voucherCount,
+                Latitude = latitude,
+                Longitude = longitude,
+                Timestamp = timestamp
+            };
+            try {
+                var result = await instrument.RequestVouchers(new VoucherCreatePayload.VoucherInfo[] {
+                    voucherInfo
+                });
+                return Content($"{result.Link} {result.Password}");
+            }
+            catch(Exception ex) {
+                _logger.LogError(ex, "Failed to request vouchers");
+                
+                ErrorMessage = "Failed to request vouchers";
+                return RedirectToAction(nameof(ShowGenerationForm), new { sourceId });
+            }
+        }
+
+        private static string GetKey(SourceLoginResultV1 loginResult, string sourceId) {
+            foreach (var s in loginResult.Sources) {
+                if (s.Id == sourceId) {
+                    return s.PrivateKey;
+                }
+            }
+
+            return null;
         }
 
     }
---
+++ src/VoucherGenerationService/VoucherGenerationService/ViewModels/GenerationFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WomPlatform.Web.Generator.ViewModels {
+    
+    public class GenerationFormViewModel {
+
+        public List<Connector.Models.AimListResponseV2.Aim> Aims { get; set; }
+
+    }
+
+}
